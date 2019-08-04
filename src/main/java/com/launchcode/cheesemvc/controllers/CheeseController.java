1508f0e5e12ace5472c606af75d96fc9631b2adb
lindskelsey <lindsaykelsey@gmail.com>
2019-08-04 16:00:38 -0500

    intro to views pt. 2

--- src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,19 +1,43 @@
-package com.launchcode.cheesemvc.controllers;
+package org.launchcode.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
+
     // Request path: cheese/
     @RequestMapping(value = "")
     public String index(Model model) {
 
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
+
+    // Request path: cheese/add
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    // Request path: cheese/add
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+
+        cheeses.add(cheeseName);
+
+        // Redirect to cheese/
+        return "redirect:";
+    }
+
 }
