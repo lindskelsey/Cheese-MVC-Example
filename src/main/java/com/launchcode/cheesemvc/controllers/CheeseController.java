dca66681b55fc08c94ea6221bb49deab7e1ef519
lindskelsey <lindsaykelsey@gmail.com>
2019-08-05 13:19:36 -0500

    prep work and studio 3.4 solutions

--- src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,4 +1,4 @@
-package org.launchcode.controllers;
+package com.launchcode.cheesemvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,38 +6,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
-
-    // Request path: cheese/
+    static HashMap<String, String> cheeses = new HashMap<>();
+    // request path: /cheese
     @RequestMapping(value = "")
-    public String index(Model model) {
+    public String index(Model model){
 
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
 
-    // Request path: cheese/add
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseForm(Model model) {
-
+    public String displayAddCheeseForm(Model model){
         model.addAttribute("title", "Add Cheese");
         return "cheese/add";
     }
 
-    // Request path: cheese/add
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-
-        cheeses.add(cheeseName);
-
-        // Redirect to cheese/
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
+        cheeses.put(cheeseName, cheeseDescription);
         return "redirect:";
     }
 
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model){
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam String cheese){
+        cheeses.remove(cheese);
+        return "redirect:";
+    }
 }
